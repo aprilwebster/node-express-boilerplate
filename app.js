3d498fe77f20657cb5d8854d57121a0718f2a831
April Webster <awebster@us.ibm.com>
2018-10-28 18:26:19 -0700

    logging: adding the winston general purpose logging middleware

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require('express');
 
+var logger = require('./config/winston');
+
 const app = express();
 
 // Bootstrap application settings
 require('./config/express')(app);
+
+logger.info('Hello world');
+logger.debug('Debugging info');
 
 /**
  * entry point for the web app
