6c56605cd8cd2ae6b1e6d37d9bbf19fcbe3c14a9
April Webster <awebster@us.ibm.com>
2018-10-29 00:43:00 -0700

    logging: adding morgan

--- app.js
+++ app.js
@@ -1,20 +1,33 @@
 const express = require('express');
 
-var logger = require('./config/winston');
+// var logger = require('./config/winston');
+var morgan = require('morgan');
+var path = require('path');
+var fs = require('fs');
 
 const app = express();
 
 // Bootstrap application settings
 require('./config/express')(app);
 
-logger.info('Hello world');
-logger.debug('Debugging info');
+// const morganFormat = ': method: url: status: response - time ms -: res[content - length]';
+morgan(':method :url :status :res[content-length] - :response-time ms');
+
+// create a write stream (in append mode)
+var accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'));
+
+// setup the logger
+app.use(morgan('combined', {
+  stream: accessLogStream
+}));
+
 
 /**
  * entry point for the web app
  */
 app.get('/', (req, res) => {
-  res.render('index');
+  // res.render('index');
+  res.send('hello world');
 });
 
 module.exports = app;
--- server.js
+++ server.js
@@ -3,8 +3,11 @@
 const server = require('./app');
 const port = 3000;
 
+var logger = require('./config/winston');
+
 server.listen(port, () => {
-  console.log('Server running on port: %d', port);
+  logger.info(`Starting server on port: ${port}`);
+  // console.log('Server running on port: %d', port);
 });
 
 
